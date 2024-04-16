cbf4ad557f47e56aac373bc373411fed15ac1fca
244852437 <63935246+244852437@users.noreply.github.com>
2024-04-16 19:57:13 +0800

    Add files via upload

---
+++ assets/js/download.js
@@ -0,0 +1,37 @@
+// JavaScript 函数用于滚动页面到顶部
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // 平滑滚动
+    });
+}
+
+// 监听页面滚动事件，控制按钮显示与隐藏
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var backToTopButton = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
+document.querySelectorAll('.tree details summary').forEach(function (summary) {
+    summary.addEventListener('click', function () {
+        // 获取点击的details元素
+        var details = this.parentElement;
+
+        // 获取与点击的details同级的所有details元素
+        var siblings = details.parentElement.querySelectorAll('details');
+
+        // 遍历所有同级details元素，关闭除了点击的之外的其他details
+        siblings.forEach(function (sibling) {
+            if (sibling !== details) {
+                sibling.removeAttribute('open');
+            }
+        });
+    });
+});
